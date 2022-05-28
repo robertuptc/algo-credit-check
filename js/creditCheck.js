a15ae774370c82b05422bac739f017ab20cdf71c
robertuptc <robertuptc@gmail.com>
2022-05-27 20:59:32 -0400

    finished exercise

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,2 +1,16 @@
-exports.creditCheck = function(str) {
+exports.creditCheck = function (str) {
+    let strSplit = str.split('')
+    let strToNumArr = strSplit.map(elem => parseInt(elem))
+
+    for (let i = 0; i < strToNumArr.length; i = i + 2) {
+        if (strToNumArr[i] * 2 > 9) {
+            strToNumArr[i] = (strToNumArr[i] * 2).toString().split('').map(elem => parseInt(elem)).reduce((a, b) => a + b)
+        } else {
+            strToNumArr[i] = strToNumArr[i] * 2
+        }
+    }
+
+    return strToNumArr.reduce((a, b) => a + b) % 10 === 0 ? "The number is valid!" : "The number is invalid!"
 }
+
+
--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,13 @@
-def credit_check(str):
-    pass
+from posixpath import split
+import functools
 
-# Your Luhn Algorithm Here
-# Expected Output:
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
+def credit_check(string):
+    split_str = list(map(int,str(string)))
+
+    for i in range(0, len(split_str), 2):
+        if split_str[i] * 2 > 9:
+            split_str[i] = functools.reduce(lambda agg, item : agg + item, list(map(int,str(split_str[i] * 2))))
+        else :
+            split_str[i] = split_str[i] * 2
+    return "The number is valid!" if functools.reduce(lambda agg, item : agg + item, split_str) % 10 == 0 else "The number is invalid!"
 
